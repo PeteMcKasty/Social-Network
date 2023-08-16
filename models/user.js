8afd824aa1762d32f0970b1bae45937ec3c8c2d7
Peter McKasty <pmck25@yahoo.com>
2023-08-16 19:38:10 -0400

    started user model

--- models/user.js
+++ models/user.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  // Define user schema fields
+  username: {
+    type: String,
+  }
 });
 
 const User = mongoose.model('User', userSchema);
