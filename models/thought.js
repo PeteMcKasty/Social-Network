d57b059c7c766241027c7c5cbc8cce624e314224
Peter McKasty <pmck25@yahoo.com>
2023-08-16 19:48:08 -0400

    finished user and thoughts models

--- models/thought.js
+++ models/thought.js
@@ -2,7 +2,16 @@
 const mongoose = require('mongoose');
 
 const thoughtSchema = new mongoose.Schema({
-  // Define thought schema fields
+  thoughtText: {
+    type: String,
+    required: true,
+    trim: true,
+    maxLength: 280,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
 });
 
 const Thought = mongoose.model('Thought', thoughtSchema);
--- models/user.js
+++ models/user.js
@@ -3,7 +3,16 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   username: {
     type: String,
-  }
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
 });
 
 const User = mongoose.model('User', userSchema);
