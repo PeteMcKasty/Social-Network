e826488a6438f8a4c76457ed334991cc085b3c7d
Peter McKasty <pmck25@yahoo.com>
2023-08-16 19:08:38 -0400

    added create function for thoughts controller

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -1,3 +1,11 @@
 const Thought = require('../models/Thought');
 
-// Define controller functions for CRUD operations on thoughts
+// Controller function to create a new thought
+const createThought = async (req, res) => {
+  try {
+    const newThought = await Thought.create(req.body);
+    res.json(newThought);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+};
