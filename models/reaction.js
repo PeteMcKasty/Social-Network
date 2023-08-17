69e27549ebd0dac51b410dc86e7c86b4ee34920f
Peter McKasty <pmck25@yahoo.com>
2023-08-16 20:55:58 -0400

    created reaction.js model to match required criteria

---
+++ models/reaction.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const reactionSchema = new mongoose.Schema({
+  reactionId: {
+    type: mongoose.Schema.Types.ObjectId,
+    default: () => new mongoose.Types.ObjectId(),
+  },
+  reactionBody: {
+    type: String,
+    required: true,
+    maxLength: 280,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (timestamp) => formatDate(timestamp), // You need to define this function
+  },
+});
+
+const Reaction = mongoose.model('Reaction', reactionSchema);
+
+module.exports = Reaction;
