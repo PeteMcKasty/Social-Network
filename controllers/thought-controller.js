f99144f22e457a46b7ca14da3e9b90e0cc4d97da
Peter McKasty <pmck25@yahoo.com>
2023-08-16 21:17:58 -0400

    updated thought and user controllers to match required criteria

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -9,3 +9,42 @@ const createThought = async (req, res) => {
     res.status(400).json(error);
   }
 };
+
+// Controller function to create a new reaction for a thought
+const createReaction = async (req, res) => {
+    try {
+      const newReaction = await Reaction.create(req.body);
+      const updatedThought = await Thought.findByIdAndUpdate(
+        req.params.thoughtId,
+        { $push: { reactions: newReaction } },
+        { new: true }
+      );
+      res.json(updatedThought);
+    } catch (error) {
+      res.status(400).json(error);
+    }
+  };
+  
+  // Controller function to delete a reaction from a thought
+  const deleteReaction = async (req, res) => {
+    try {
+      const updatedThought = await Thought.findByIdAndUpdate(
+        req.params.thoughtId,
+        { $pull: { reactions: { reactionId: req.params.reactionId } } },
+        { new: true }
+      );
+      res.json(updatedThought);
+    } catch (error) {
+      res.status(400).json(error);
+    }
+  };
+  
+  module.exports = {
+    createThought,
+    updateThought,
+    deleteThought,
+    getAllThoughts,
+    getThoughtById,
+    createReaction,
+    deleteReaction,
+  };
--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -9,3 +9,41 @@ const createUser = async (req, res) => {
     res.status(400).json(error);
   }
 };
+
+// Controller function to add a new friend to a user's friend list
+const addFriend = async (req, res) => {
+    try {
+      const updatedUser = await User.findByIdAndUpdate(
+        req.params.userId,
+        { $addToSet: { friends: req.params.friendId } },
+        { new: true }
+      );
+      res.json(updatedUser);
+    } catch (error) {
+      res.status(400).json(error);
+    }
+  };
+  
+  // Controller function to remove a friend from a user's friend list
+  const removeFriend = async (req, res) => {
+    try {
+      const updatedUser = await User.findByIdAndUpdate(
+        req.params.userId,
+        { $pull: { friends: req.params.friendId } },
+        { new: true }
+      );
+      res.json(updatedUser);
+    } catch (error) {
+      res.status(400).json(error);
+    }
+  };
+  
+  module.exports = {
+    createUser,
+    updateUser,
+    deleteUser,
+    getAllUsers,
+    getUserById,
+    addFriend,
+    removeFriend,
+  };
