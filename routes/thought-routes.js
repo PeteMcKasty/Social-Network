04947849c4c14609dfe0e1e7c421fd0d5c66cf1c
Peter McKasty <pmck25@yahoo.com>
2023-08-16 21:30:30 -0400

    updated routes

--- routes/thought-routes.js
+++ routes/thought-routes.js
@@ -1,8 +1,22 @@
 const express = require('express');
-const thoughtController = require('../controllers/thought-controller');
+const {
+  createThought,
+  updateThought,
+  deleteThought,
+  getAllThoughts,
+  getThoughtById,
+  createReaction,
+  deleteReaction,
+} = require('../controllers/thought-controller');
 
 const router = express.Router();
 
-router.post('/thoughts', thoughtController.createThought);
+router.get('/thoughts', getAllThoughts);
+router.get('/thoughts/:thoughtId', getThoughtById);
+router.post('/thoughts', createThought);
+router.put('/thoughts/:thoughtId', updateThought);
+router.delete('/thoughts/:thoughtId', deleteThought);
+router.post('/thoughts/:thoughtId/reactions', createReaction);
+router.delete('/thoughts/:thoughtId/reactions/:reactionId', deleteReaction);
 
 module.exports = router;
--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,8 +1,22 @@
 const express = require('express');
-const userController = require('../controllers/user-controller');
+const {
+  createUser,
+  updateUser,
+  deleteUser,
+  getAllUsers,
+  getUserById,
+  addFriend,
+  removeFriend,
+} = require('../controllers/user-controller');
 
 const router = express.Router();
 
-router.post('/users', userController.createUser);
+router.get('/users', getAllUsers);
+router.get('/users/:userId', getUserById);
+router.post('/users', createUser);
+router.put('/users/:userId', updateUser);
+router.delete('/users/:userId', deleteUser);
+router.post('/users/:userId/friends/:friendId', addFriend);
+router.delete('/users/:userId/friends/:friendId', removeFriend);
 
 module.exports = router;
