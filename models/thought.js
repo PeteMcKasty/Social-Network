9447794bf5109b754964302e7b3906c5da738e68
Peter McKasty <pmck25@yahoo.com>
2023-08-16 20:33:26 -0400

    updated thoughts model to represent required criteria

--- models/thought.js
+++ models/thought.js
@@ -1,4 +1,3 @@
-// models/Thought.js
 const mongoose = require('mongoose');
 
 const thoughtSchema = new mongoose.Schema({
@@ -8,10 +7,20 @@ const thoughtSchema = new mongoose.Schema({
     trim: true,
     maxLength: 280,
   },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (timestamp) => formatDate(timestamp), // You need to define this function
+  },
   username: {
     type: String,
     required: true,
   },
+  reactions: [reactionSchema], // You need to define reactionSchema
+});
+
+thoughtSchema.virtual('reactionCount').get(function () {
+  return this.reactions.length;
 });
 
 const Thought = mongoose.model('Thought', thoughtSchema);
