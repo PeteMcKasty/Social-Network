964b9d1c68d49d56b872d2cd34069605356e81c9
Peter McKasty <pmck25@yahoo.com>
2023-08-16 20:10:15 -0400

    finished server.js by creating middleware and mongoDB connection, as well as port connection and use methods

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
+const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.json());
+
+// Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -9,4 +17,13 @@ mongoose.connect(process.env.MONGODB_URI, {
 const db = mongoose.connection;
 db.once('open', () => {
   console.log('Connected to MongoDB database');
+});
+
+// Routes
+app.use('/api', require('./routes/user-routes'));
+app.use('/api', require('./routes/thought-routes'));
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
